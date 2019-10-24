a67e22d31dcb1c8592c7da66eefe32da9d858eed
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2019-10-24 11:33:20 +0100

    Add controller methods for organisation details submission
    This implements the submission behaviour for the EU Funding Registration form.  Entry is
    stored in the session variables and user is redirected to the next step.

---
+++ app/controllers/funding_form/grant_agreement_number_controller.rb
@@ -0,0 +1,5 @@
+class FundingForm::GrantAgreementNumberController < Application Controller
+  def show; end
+
+  def submit; end
+end
--- app/controllers/funding_form/organisation_details_controller.rb
+++ app/controllers/funding_form/organisation_details_controller.rb
@@ -1,5 +1,13 @@
 class FundingForm::OrganisationDetailsController < ApplicationController
-  def show; end
+  def show
+    render "funding_form/organisation_details"
+  end
 
-  def submit; end
+  def submit
+    keys = %i[organisation_name company_house_or_charity_commission_number address_line_1 address_line_2 address_town address_county address_postcode]
+    keys.each do |key|
+      session[key] = params[key]
+    end
+    redirect_to controller: "funding_form/grant_agreement_number", action: "show"
+  end
 end
